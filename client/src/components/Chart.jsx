eac06d35cc21987bb5e862287703a1ebb1db5c94
Waleed Shabbir <wshabbir@eis-mainz.iais.fraunhofer.de>
2024-01-24 15:37:20 +0100

    chart component prettify

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
-import L from 'leaflet'; // Import Leaflet
-
-/* eslint-disable import/first */
-let backend = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/api';
+import L from 'leaflet';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -15,6 +15,7 @@ import {
   PointElement,
   LineController,
   ArcElement,
+  LineElement
 } from 'chart.js';
 
 ChartJS.register(
@@ -26,14 +27,13 @@ ChartJS.register(
   Legend,
   PointElement,
   LineController,
-  ArcElement
+  ArcElement,
+  LineElement
 );
 
-
-
+let backend = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/api';
 
 const getCityCoordinates = async (cityName) => {
-
   let bearer = 'Bearer ' + window.localStorage.getItem("token");
   try {
     const response = await fetch(backend + '/misc/getCoordinates', {
@@ -55,9 +55,133 @@ const getCityCoordinates = async (cityName) => {
 
 const Chart = ({ data }) => {
   const mapContainerRef = useRef(null);
+  const [selectedXAxis, setSelectedXAxis] = useState('');
+  const [selectedYAxis, setSelectedYAxis] = useState('');
+  const [selectedChartType, setSelectedChartType] = useState('bar');
+
+  const handleXAxisChange = (event) => {
+    setSelectedXAxis(event.target.value);
+  };
+
+  const handleYAxisChange = (event) => {
+    setSelectedYAxis(event.target.value);
+  };
+
+  const handleChartTypeChange = (event) => {
+    setSelectedChartType(event.target.value);
+  };
+
+  const renderChartOptions = () => {
+    const properties = Object.keys(data[0] || {});
+
+    return (
+      <div className="chart-options">
+        <Form as={Row} className='mx-0'>
+          <Form.Group as={Col} controlId="xAxisSelect">
+            <Form.Label>X Axis:</Form.Label>
+            <Form.Control as="select" value={selectedXAxis} onChange={handleXAxisChange}>
+              <option value="" disabled>Select X Axis</option>
+              {properties.map((property) => (
+                <option key={property} value={property}>
+                  {property}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="yAxisSelect">
+            <Form.Label>Y Axis:</Form.Label>
+            <Form.Control as="select" value={selectedYAxis} onChange={handleYAxisChange}>
+              <option value="" disabled>Select Y Axis</option>
+              {properties.map((property) => (
+                <option key={property} value={property}>
+                  {property}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="chartTypeSelect">
+            <Form.Label>Chart Type:</Form.Label>
+            <Form.Control as="select" value={selectedChartType} onChange={handleChartTypeChange}>
+              <option value="bar">Bar Chart</option>
+              <option value="line">Line Chart</option>
+              <option value="pie">Pie Chart</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  };
+
+  const generateChartData = () => {
+    const xValues = data.map((item) => item[selectedXAxis]);
+    const yValues = data.map((item) => item[selectedYAxis]);
+
+    const chartData = {
+      labels: xValues,
+      datasets: [
+        {
+          label: selectedYAxis,
+          data: yValues,
+          backgroundColor: generateRandomColors(yValues.length),
+        },
+      ],
+    };
+
+    return chartData;
+  };
+
+  const generateRandomColors = (numColors) => {
+    const colors = [];
+    for (let i = 0; i < numColors; i++) {
+      const color = `rgba(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, 0.8)`;
+      colors.push(color);
+    }
+    return colors;
+  };
+
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const renderCharts = () => {
+    if (!data || data.length === 0) {
+      return <p>Loading data...</p>;
+    }
+
+    const chartData = generateChartData();
+    const chartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    };
+
+    let ChartComponent;
+    if (selectedChartType === 'pie') {
+      ChartComponent = Pie;
+    } else if (selectedChartType === 'bar') {
+      ChartComponent = Bar;
+    } else if (selectedChartType === 'line') {
+      ChartComponent = Line;
+    }
+
+    return (
+      <div className="chart">
+        {renderChartOptions()}
+
+        {selectedXAxis && selectedYAxis && selectedChartType && <h3>{`${selectedXAxis} vs ${selectedYAxis} (${selectedChartType.toUpperCase()} Chart)`}</h3>
+        }
+        {/* <h3>{`${selectedXAxis} vs ${selectedYAxis} (${selectedChartType.toUpperCase()} Chart)`}</h3> */}
+        <ChartComponent data={chartData} options={chartOptions} />
+        <p>Description</p>
+      </div>
+    );
+  };
 
   const renderMap = () => {
-
     if (!data || data.length === 0 || !data.some(item => item.location || item.Location)) {
       return null; // Don't render the map if there's no data or no 'location' property
     }
@@ -70,131 +194,43 @@ const Chart = ({ data }) => {
       </div>
     );
   };
+
   useEffect(() => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css';
-    // Append the link element to the head of the document
     document.head.appendChild(link);
-    // Initialize the Leaflet map once the map container is available in the DOM
+
     if (mapContainerRef.current && data.some(item => item.location || item.Location)) {
       if (!mapContainerRef.current._leaflet_id) {
-        // Create the Leaflet map
-        const map = L.map(mapContainerRef.current).setView([48.8566, 2.3522], 3); // Set initial coordinates and zoom level
+        const map = L.map(mapContainerRef.current).setView([48.8566, 2.3522], 3);
 
-        // Add map tiles (you'll need to provide a tile layer URL)
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
           maxZoom: 19
         }).addTo(map);
 
-        // Loop through your data and add markers for city names
         data.forEach(async (item) => {
           try {
             if (item.location || item.Location) {
-              // Get latitude and longitude using the getCityCoordinates function
               const { latitude, longitude } = await getCityCoordinates(item.location || item.Location);
 
-              // Add a marker for the city with its latitude and longitude
               if (latitude && longitude) {
                 const cityMarker = L.marker([latitude, longitude]).addTo(map);
                 cityMarker.bindPopup(item.location || item.Location);
               }
             }
-            // Display city name on marker click
           } catch (error) {
             console.error(`Error fetching coordinates for "${item.location}": ${error.message}`);
           }
         });
         mapContainerRef.current._leaflet_id = map._leaflet_id;
       }
-
     }
   }, [data]);
 
-  const renderCharts = () => {
-    if (!data || data.length === 0) {
-      return <p>Loading data...</p>;
-    }
-
-    const chartComponents = [];
-
-    // Generate charts for each property dynamically
-    Object.keys(data[0]).forEach((property) => {
-      if (property !== "location" || property !== "Location") {
-        const chartData = generateChartData(property);
-
-        const chartComponent = generateChartComponent(property, chartData);
-        chartComponents.push(chartComponent);
-      }
-    });
-
-    return chartComponents;
-  };
-
-  const generateChartData = (property) => {
-    const values = data.map((item) => item[property]);
-    const uniqueValues = [...new Set(values)];
-
-    const chartData = {
-      labels: uniqueValues,
-      datasets: [
-        {
-          label: property,
-          data: uniqueValues.map(() => 1),
-          backgroundColor: generateRandomColors(uniqueValues.length),
-        },
-      ],
-    };
-
-    return chartData;
-  };
-
-  // Helper function to generate random colors
-  const generateRandomColors = (numColors) => {
-    const colors = [];
-    for (let i = 0; i < numColors; i++) {
-      const color = `rgba(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, 0.8)`;
-      colors.push(color);
-    }
-    return colors;
-  };
-
-  // Helper function to generate a random number between min and max (inclusive)
-  const getRandomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  const generateChartComponent = (property, chartData) => {
-    const chartOptions = {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    };
-
-    let ChartComponent;
-    if (typeof chartData.labels[0] === 'string') {
-      ChartComponent = Pie;
-    } else if (typeof chartData.labels[0] === 'number') {
-      ChartComponent = Bar;
-    } else {
-      ChartComponent = Line;
-    }
-
-    return (
-      <div key={property}  className={property}>
-        <h3>{property} Chart</h3>
-        <ChartComponent data={chartData} options={chartOptions} className={property} />
-        <p>{property}</p>
-      </div>
-    );
-  };
-
   return (
     <>
-      {/* Render the charts */}
       {renderCharts()}
       {renderMap()}
     </>
